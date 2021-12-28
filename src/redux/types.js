d98fc1042fa29fb78ee8376f821bf43ba1bc627b
Oystein Koder <85921431+OysteinKoder@users.noreply.github.com>
2021-12-28 13:18:15 +0100

    Types to be used for doing spesific api calls on that token.

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,17 +1,21 @@
 export const GET_COINS_DATA = 'GET_COINS_DATA';
-export const COINS_DATA_ERROR = 'COINS_DATA_ERROR';
+export const DATA_ERROR = 'DATA_ERROR';
 
 export const TOGGLE_LOGIN = 'TOGGLE_LOGIN';
 export const IS_LOGGED_OUT = 'IS_LOGGED_OUT';
 
+export const GET_BTC_DATA = 'GET_BTC_DATA'
 export const BUY_BTC = 'BUY_BTC';
 export const SELL_BTC = 'SELL_BTC';
 
+export const GET_ETH_DATA = 'GET_ETH_DATA'
 export const BUY_ETH = 'BUY_ETH';
 export const SELL_ETH = 'SELL_ETH';
 
+export const GET_DOGE_DATA = 'GET_DOGE_DATA'
 export const BUY_DOGE = 'BUY_DOGE';
 export const SELL_DOGE = 'SELL_DOGE';
 
+export const GET_BNB_DATA = 'GET_BNB_DATA'
 export const BUY_BNB = 'BUY_BNB';
 export const SELL_BNB = 'SELL_BNB';
