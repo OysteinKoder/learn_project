1e5cc509f682e22ec6336fd9c29ee6d64ba778b0
Oystein Koder <85921431+OysteinKoder@users.noreply.github.com>
2021-12-28 11:07:24 +0100

    added a wallet reducer that i try to feed info about only the used coins into

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import coinReducers from "./coinReducers";
 import loggedInReducer from "./isLoggedIn"
+import walletReducer from "./walletReducer";
 
 export default combineReducers({
     coins: coinReducers,
-    loggedIn: loggedInReducer
+    loggedIn: loggedInReducer,
+    BTC_VALUE: walletReducer
 })
