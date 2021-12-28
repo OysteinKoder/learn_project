b0b1c1837dd2eb74fc45b899d38e215901fda51c
Oystein Koder <85921431+OysteinKoder@users.noreply.github.com>
2021-12-28 11:09:08 +0100

    added a bunch of actions to be used in the wallet reducer (perhaps)

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,7 +1,17 @@
 export const GET_COINS_DATA = 'GET_COINS_DATA';
-export const COINS_DATA_ERROR = 'COINS_DATA_ERROR'
+export const COINS_DATA_ERROR = 'COINS_DATA_ERROR';
 
-export const IS_LOGGED_IN = 'IS_LOGGED_IN'
-export const IS_LOGGED_OUT = 'IS_LOGGED_OUT'
+export const TOGGLE_LOGIN = 'TOGGLE_LOGIN';
+export const IS_LOGGED_OUT = 'IS_LOGGED_OUT';
 
-export const UPDATE_WALLET = 'USD_VALUE'
+export const BUY_BTC = 'BUY_BTC';
+export const SELL_BTC = 'SELL_BTC';
+
+export const BUY_ETH = 'BUY_ETH';
+export const SELL_ETH = 'SELL_ETH';
+
+export const BUY_DOGE = 'BUY_DOGE';
+export const SELL_DOGE = 'SELL_DOGE';
+
+export const BUY_BNB = 'BUY_BNB';
+export const SELL_BNB = 'SELL_BNB';
