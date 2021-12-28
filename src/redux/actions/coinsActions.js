545462e71247ec39377509299c22dff33bb7996a
Oystein Koder <85921431+OysteinKoder@users.noreply.github.com>
2021-12-28 14:13:43 +0100

    added actions that get for the selected coins

--- src/redux/actions/coinsActions.js
+++ src/redux/actions/coinsActions.js
@@ -1,18 +1,106 @@
-import { GET_COINS_DATA, COINS_DATA_ERROR } from "../types";
+import {
+GET_COINS_DATA,
+DATA_ERROR,
+GET_BTC_DATA,
+GET_ETH_DATA,
+GET_DOGE_DATA,
+GET_BNB_DATA,
+GET_USD_DATA,
+ } from "../types";
 import axios from 'axios';
 
 export const getCoins = () => async dispatch => {
-    try{
+    try {
         const res = await axios.get('https://api.coinlore.net/api/tickers/')
         dispatch({
             type: GET_COINS_DATA,
             payload: res.data
         })
     }
+    catch (e) {
+        dispatch({
+            type: DATA_ERROR,
+            payload: console.log(e)
+        })
+    }
+}
+
+export const getUsd = () => async dispatch => {
+    try{
+        const res = await axios.get('https://api.coinlore.net/api/ticker/?id=518')
+        dispatch({
+            type: GET_USD_DATA,
+            payload: res.data
+        })
+    }
+    catch(e) {
+        dispatch({
+            type: DATA_ERROR,
+            payload: console.log(e)
+        })
+    }
+}
+
+export const getBtc = () => async dispatch => {
+    try {
+        const res = await axios.get('https://api.coinlore.net/api/ticker/?id=90')
+        dispatch({
+            type: GET_BTC_DATA,
+            payload: res.data
+        })
+    }
+    catch (e) {
+        dispatch({
+            type: DATA_ERROR,
+            payload: console.log(e)
+        })
+    }
+}
+
+export const getEth = () => async dispatch => {
+    try{
+        const res = await axios.get('https://api.coinlore.net/api/ticker/?id=80')
+        dispatch({
+            type: GET_ETH_DATA,
+            payload: res.data
+        })
+    }
+    catch(e) {
+        dispatch({
+            type: DATA_ERROR,
+            payload: console.log(e)
+        })
+    }
+}
+
+export const getDoge = () => async dispatch => {
+    try{
+        const res = await axios.get('https://api.coinlore.net/api/ticker/?id=2')
+        dispatch({
+            type: GET_DOGE_DATA,
+            payload: res.data
+        })
+    }
     catch(e){
         dispatch({
-            type: COINS_DATA_ERROR,
+            type: DATA_ERROR,
             payload: console.log(e)
+        })
+    }
+}
+
+export const getBnb = () => async dispatch => {
+    try{
+        const res = await axios.get('https://api.coinlore.net/api/ticker/?id=2710')
+        dispatch({
+            type: GET_BNB_DATA,
+            payload: res.data
+        })
+    }
+    catch(e){
+        dispatch({
+            type:DATA_ERROR,
+            payload:console.log(e)
         })
     }
 }
