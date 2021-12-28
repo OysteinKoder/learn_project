14ef9b4746e334dec298248135a7093bc0562fee
Oystein Koder <85921431+OysteinKoder@users.noreply.github.com>
2021-12-28 11:07:50 +0100

    reducer for the wallet state

--- src/redux/reducers/walletReducer.js
+++ src/redux/reducers/walletReducer.js
@@ -1,12 +1,28 @@
-import { UPDATE_WALLET } from "../types";
+import { BUY_BTC } from "../types";
+import { SELL_BTC } from "../types";
+import { store } from "../store";
+
 
 const initialWallet = {
-    USD: null,
+    USD: 1000,
     BTC: null,
     ETH: null,
     DOGE: null,
     BNB: null,
 }
+
+export default function (state = initialWallet, action) {
+    
+    switch (action.type) {
+        case BUY_BTC:
+            return {
+                ...state, 
+                BTC: action.payload 
+            }
+            default: return state
+    }
+}
+
 
 // USD: id "518"
 // BTC: id "90"
