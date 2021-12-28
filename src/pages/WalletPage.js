12e73efefd164250ee82552a8ef083f88610338c
Oystein Koder <85921431+OysteinKoder@users.noreply.github.com>
2021-12-28 14:12:59 +0100

    added text to display current wallet info

--- src/pages/WalletPage.js
+++ src/pages/WalletPage.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { store } from '../redux/store';
+import { useSelector } from 'react-redux';
 
-export const WalletPage = () => (
-    <div>IM a wallet</div>
-)
+export const WalletPage = () => {
+    const currentWallet = useSelector(state => state.wallet)
+    if (currentWallet) 
+    {return(
+        <div>
+    <p>USD:${currentWallet.USD}</p>
+    <p>BTC: {currentWallet.BTC}</p>
+    <p>ETH: {currentWallet.ETH}</p>
+    <p>DOGE:{currentWallet.DOGE}</p>
+    <p>BNB: {currentWallet.BNB}</p>
+    </div>
+)}}
