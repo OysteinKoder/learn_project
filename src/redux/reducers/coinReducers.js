792d35bc48e25789638fa2ffc045e3dda31b6416
Oystein Koder <85921431+OysteinKoder@users.noreply.github.com>
2021-12-29 13:24:47 +0100

    added a bunch of cases that returns the data from the spesific coin call

--- src/redux/reducers/coinReducers.js
+++ src/redux/reducers/coinReducers.js
@@ -1,8 +1,20 @@
-import { GET_COINS_DATA } from "../types";
+import { init } from "i18n";
+import { 
+    GET_COINS_DATA,
+    GET_USD_DATA,
+    GET_BTC_DATA,
+    GET_ETH_DATA,
+    GET_DOGE_DATA,
+    GET_BNB_DATA,
+    CLEAN_STATE
+} from "../types";
 
-const initialState = {
+
+let loadingState = false;
+
+export const initialState = {
     coins:[],
-    loading:true,
+    loading:loadingState,
 }
 
 export default function (state = initialState, action){
@@ -10,9 +22,46 @@ export default function (state = initialState, action){
         case GET_COINS_DATA:
         return {
             ...state,
-            coins: action.payload,
-            loading: false
+            coins: [state.coins, action.payload] 
         }
+        case GET_BTC_DATA: 
+        return {
+            ...state,
+            coins: state.coins.concat(action.payload)
+        }
+        case GET_USD_DATA:
+            return {
+                ...state,
+                coins: state.coins.concat(action.payload),
+                loading: !loadingState
+            }
+        case GET_ETH_DATA:
+            return{
+                ...state,
+                coins: state.coins.concat(action.payload),
+                loading: !loadingState
+            }
+        case GET_ETH_DATA:
+            return{
+                ...state,
+            coins: state.coins.concat(action.payload),
+            loading: !loadingState
+        }
+        case GET_DOGE_DATA: 
+            return {
+                ...state,
+                coins: state.coins.concat(action.payload),
+                loading: !loadingState
+            }
+        case GET_BNB_DATA: 
+        return {
+            ...state,
+            coins: state.coins.concat(action.payload),
+            loading: !loadingState
+        }
+        case CLEAN_STATE: 
+        return initialState
+        
         default: return state
     }
 }
