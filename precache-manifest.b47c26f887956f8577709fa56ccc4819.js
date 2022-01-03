33f84ac8ff84c91e9d7616d8e4202e9deb8ab928
OysteinKoder <oystein.kodehode@gmail.com>
2022-01-03 07:36:59 +0100

    Updates

---
+++ precache-manifest.b47c26f887956f8577709fa56ccc4819.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1030a377ab5c5a21e9c2d7e084ea7b4",
+    "url": "/learn_project/index.html"
+  },
+  {
+    "revision": "5965af71ff19874422b2",
+    "url": "/learn_project/static/css/2.ca3a22af.chunk.css"
+  },
+  {
+    "revision": "3dd8b0f8d514dcee6c86",
+    "url": "/learn_project/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "5965af71ff19874422b2",
+    "url": "/learn_project/static/js/2.af63b660.chunk.js"
+  },
+  {
+    "revision": "2ed016325696034351f5",
+    "url": "/learn_project/static/js/3.662db00f.chunk.js"
+  },
+  {
+    "revision": "3dd8b0f8d514dcee6c86",
+    "url": "/learn_project/static/js/main.1fd7b9e1.chunk.js"
+  },
+  {
+    "revision": "7453ef1445f52bc51cda",
+    "url": "/learn_project/static/js/runtime-main.1f3bf831.js"
+  },
+  {
+    "revision": "4ed4e2952e690f3318e3e13d1789cd9c",
+    "url": "/learn_project/static/media/DNB-Bold.4ed4e295.ttf"
+  },
+  {
+    "revision": "7efbd636d6779a78ca6e01dc5425e942",
+    "url": "/learn_project/static/media/DNB-Bold.7efbd636.woff"
+  },
+  {
+    "revision": "a461a568eb5db2b8d6d190a6c74997ee",
+    "url": "/learn_project/static/media/DNB-Bold.a461a568.woff2"
+  },
+  {
+    "revision": "3cd8dafac230303bce95f6e0b99d4ef8",
+    "url": "/learn_project/static/media/DNB-Medium.3cd8dafa.woff"
+  },
+  {
+    "revision": "4ef22dcf97f389c669bcd6e829e5baaa",
+    "url": "/learn_project/static/media/DNB-Medium.4ef22dcf.ttf"
+  },
+  {
+    "revision": "e799cbd89641be827e14d9ded0578e6c",
+    "url": "/learn_project/static/media/DNB-Medium.e799cbd8.woff2"
+  },
+  {
+    "revision": "448a75f707b31eae39f22e0ba84c22c1",
+    "url": "/learn_project/static/media/DNB-Regular.448a75f7.ttf"
+  },
+  {
+    "revision": "9684ae7fd43ccd0ffce1eee01f1a04ef",
+    "url": "/learn_project/static/media/DNB-Regular.9684ae7f.woff2"
+  },
+  {
+    "revision": "9d8396fcd423b86ff1987050ac42cdcd",
+    "url": "/learn_project/static/media/DNB-Regular.9d8396fc.woff"
+  },
+  {
+    "revision": "4707d625043257aff61f53dc0cf6ead0",
+    "url": "/learn_project/static/media/DNB-Skeleton-Bold.4707d625.woff"
+  },
+  {
+    "revision": "681aaac5c408977d73d4e0d6403c4eea",
+    "url": "/learn_project/static/media/DNB-Skeleton-Bold.681aaac5.ttf"
+  },
+  {
+    "revision": "6c1b0d1f83fb534a6fbe1fdb89a21b5b",
+    "url": "/learn_project/static/media/DNB-Skeleton-Bold.6c1b0d1f.woff2"
+  },
+  {
+    "revision": "3bdcde46d30a5c7878eaf197f5555a1b",
+    "url": "/learn_project/static/media/DNB-Skeleton-Medium.3bdcde46.woff"
+  },
+  {
+    "revision": "4a37a954fe9e3fd94aebdbdb46b04d41",
+    "url": "/learn_project/static/media/DNB-Skeleton-Medium.4a37a954.ttf"
+  },
+  {
+    "revision": "5ec7bfd6ff554b76131581c5ba0d990c",
+    "url": "/learn_project/static/media/DNB-Skeleton-Medium.5ec7bfd6.woff2"
+  },
+  {
+    "revision": "48d8fe860294f2324c558f9f7d7d4812",
+    "url": "/learn_project/static/media/DNB-Skeleton-Regular.48d8fe86.woff2"
+  },
+  {
+    "revision": "70190e6ef08b5de2bdfa8c624df9debd",
+    "url": "/learn_project/static/media/DNB-Skeleton-Regular.70190e6e.ttf"
+  },
+  {
+    "revision": "72f96f99e37991eb5af783b025570de6",
+    "url": "/learn_project/static/media/DNB-Skeleton-Regular.72f96f99.woff"
+  },
+  {
+    "revision": "09c39e9b039177b41536256d810a1758",
+    "url": "/learn_project/static/media/DNBMono-Regular.09c39e9b.woff2"
+  },
+  {
+    "revision": "131c0e87d0480ebd3d4636995961c303",
+    "url": "/learn_project/static/media/DNBMono-Regular.131c0e87.woff"
+  },
+  {
+    "revision": "980ca46499615320b233e33bae1ea4a0",
+    "url": "/learn_project/static/media/DNBMono-Regular.980ca464.ttf"
+  },
+  {
+    "revision": "697bba5f065d8a235d3fd9bb349461cd",
+    "url": "/learn_project/static/media/DNBMono-Skeleton-Regular.697bba5f.woff2"
+  },
+  {
+    "revision": "6d41ddb4c3789dead55a8a57916c98d0",
+    "url": "/learn_project/static/media/DNBMono-Skeleton-Regular.6d41ddb4.woff"
+  },
+  {
+    "revision": "af6be20d1b17fbf798769fc2ba979be9",
+    "url": "/learn_project/static/media/DNBMono-Skeleton-Regular.af6be20d.ttf"
+  }
+]);
