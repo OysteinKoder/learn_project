bd16e81fef5cd54d573b3efb3ca9dcd59ce588d8
OysteinKoder <oystein.kodehode@gmail.com>
2022-01-05 08:44:08 +0100

    added style for top card that only shows usd wallet

--- src/components/coinPage/Styles.js
+++ src/components/coinPage/Styles.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
 export const CoinCard = styled.div`
+height: 11rem;
 display: grid;
-grid-template-columns: 15rem auto auto;
+grid-template-columns: 25rem auto auto;
+border-bottom:  3px var(--color-sea-green) solid;
+padding: 1rem;
+background-color: var(--color-mint-green-25);
+align-items: center;
+`
+
+export const TopCard = styled.div `
+height: 10rem;
+display: flex;
 border-bottom:  3px var(--color-sea-green) solid;
 padding: 1rem;
 background-color: var(--color-mint-green-25);
