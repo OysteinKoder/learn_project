32708e3deacc1afc6779f544d96d78876eb01bb5
OysteinKoder <oystein.kodehode@gmail.com>
2022-02-12 16:03:36 +0100

    updated pathing/routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import './App.css';
-import { CoinPage } from './pages/CoinPage';
+import { CoinPage } from './pages/coinPage/CoinPage';
 import { NavBar } from './components/nav_bar/NavBar';
 import { StyledBody } from './components/body_holder/bodyStyles'
-import { WalletPage } from './pages/WalletPage';
+import { WalletPage } from './pages/walletPage/WalletPage';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { AboutPage } from './pages/AboutPage';
+import { AboutPage } from './pages/aboutPage/AboutPage';
 import { H1 } from '@dnb/eufemia';
 
 
 
 function App() {
   return (
-    
+
     <StyledBody>
-    <BrowserRouter>
-      <NavBar />
-    <Routes>
-      <Route path="/wallet" element={<WalletPage/>}/>
-      <Route path="/about" element={<AboutPage/>}/>
-      <Route path="/coins" element={<CoinPage/>}/>
-      <Route path="*" element={<AboutPage/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/wallet" element={<WalletPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/coins" element={<CoinPage />} />
+          <Route path="*" element={<AboutPage />} />
+        </Routes>
+      </BrowserRouter>
     </StyledBody>
-    
+
   );
 }
 
