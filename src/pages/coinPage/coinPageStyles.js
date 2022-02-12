38fbbd88728f60bb2c0a57752f44ecaa34774a2d
OysteinKoder <oystein.kodehode@gmail.com>
2022-02-12 16:04:02 +0100

    Moved this file to new location. Styling tweaks

---
+++ src/pages/coinPage/coinPageStyles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const CoinContainer = styled.div`
+
+display: grid;
+grid-template-columns: 25rem auto auto;
+border-bottom:  3px var(--color-sea-green) solid;
+padding: 1rem;
+background-color: var(--color-mint-green-25);
+align-items: center;
+`
+
+export const LoadingSpacer = styled.div`
+height: 55rem;
+background-color: var(--color-mint-green-25);
+`
+
+export const TopCard = styled.div`
+height: 10rem;
+display: flex;
+border-bottom:  3px var(--color-sea-green) solid;
+padding: 1rem;
+background-color: var(--color-mint-green-25);
+align-items: center;
+`
